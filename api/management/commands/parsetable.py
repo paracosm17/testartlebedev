426e03f2920ed874cb31ee8a68fdbc0224b072b6
Alex (@paracosm17) <paracosm17@yandex.ru>
2024-04-07 15:48:33 +1000

    Change logic of checking if songs in DB. Removed --force arg

--- api/management/commands/parsetable.py
+++ api/management/commands/parsetable.py
@@ -10,50 +10,47 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('file', type=str, help='Path to the JSON or CSV file')
-        parser.add_argument(
-            '--force',
-            action='store_true',
-            help='Add data from the table even if there is already data in the database',
-        )
 
     def handle(self, *args, **kwargs):
         path = kwargs['file']
-        force = kwargs['force']
         extension: str = path.split(".")[-1]
+        counter = 0
 
         if extension.lower() == "json":
             with open(path, 'r') as file:
                 data = json.load(file)
-                if not force:
-                    if Song.objects.count() >= len(data):
-                        self.stdout.write(
-                            self.style.WARNING("It looks like the records have already been added to the database")
-                        )
-                        return
 
                 for song in data:
                     song_data: dict = song["data"]
-                    Song.objects.create(
-                        tags=song_data.get("tags"),
-                        theme=song_data.get("theme"),
-                        genretype=song_data.get("genretype"),
-                        genre=song_data.get("genre"),
-                        author=song_data.get("author"),
-                        composer=song_data.get("composer"),
-                        fullname=song_data.get("fullname"),
-                        creationyear=song_data.get("creationyear"),
-                    )
-            self.stdout.write(self.style.SUCCESS("Successfully imported data from JSON"))
+                    if not Song.objects.filter(
+                            tags=song_data.get("tags"),
+                            theme=song_data.get("theme"),
+                            genretype=song_data.get("genretype"),
+                            genre=song_data.get("genre"),
+                            author=song_data.get("author"),
+                            composer=song_data.get("composer"),
+                            fullname=song_data.get("fullname"),
+                            creationyear=song_data.get("creationyear"),
+                    ).exists():
+                        Song.objects.create(
+                            tags=song_data.get("tags"),
+                            theme=song_data.get("theme"),
+                            genretype=song_data.get("genretype"),
+                            genre=song_data.get("genre"),
+                            author=song_data.get("author"),
+                            composer=song_data.get("composer"),
+                            fullname=song_data.get("fullname"),
+                            creationyear=song_data.get("creationyear"),
+                        )
+                        counter += 1
+
+            self.stdout.write(self.style.SUCCESS(f"Successfully imported {counter} songs from JSON"))
+            return
 
         elif extension.lower() == "csv":
             with open(path, 'r', encoding='utf-8') as file:
                 lines = file.read().splitlines()
-                if not force:
-                    if Song.objects.count() >= len(lines[1:]):
-                        self.stdout.write(
-                            self.style.WARNING("It looks like the records have already been added to the database")
-                        )
-                        return
+
                 for line in lines[1:]:
                     if line.startswith('"') and line.endswith('"'):
                         line = line[1:-1]
@@ -61,17 +58,31 @@ class Command(BaseCommand):
                     if len(line) != 8:
                         self.stdout.write(self.style.ERROR("It looks like you have incorrect CSV table"))
                         return
-                    Song.objects.create(
-                        fullname=line[0],
-                        composer=line[1],
-                        creationyear=line[2],
-                        author=line[3],
-                        genre=line[4],
-                        genretype=line[5],
-                        theme=line[6],
-                        tags=line[7]
-                    )
-            self.stdout.write(self.style.SUCCESS("Successfully imported data from CSV"))
+
+                    if not Song.objects.filter(
+                            fullname=line[0],
+                            composer=line[1],
+                            creationyear=line[2],
+                            author=line[3],
+                            genre=line[4],
+                            genretype=line[5],
+                            theme=line[6],
+                            tags=line[7]
+                    ).exists():
+                        Song.objects.create(
+                            fullname=line[0],
+                            composer=line[1],
+                            creationyear=line[2],
+                            author=line[3],
+                            genre=line[4],
+                            genretype=line[5],
+                            theme=line[6],
+                            tags=line[7]
+                        )
+                        counter += 1
+            self.stdout.write(self.style.SUCCESS(f"Successfully imported {counter} songs from CSV"))
+            return
 
         else:
             self.stdout.write(self.style.ERROR("Wrong file format! Use json or csv"))
+            return
